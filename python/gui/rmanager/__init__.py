5570882420c3990ab808dcdbd61f39b0cc474696
Andrey Andruschushun <p0lir0id@yandex.ru>
2018-01-26 03:04:39 +0300

    credentials

--- python/gui/rmanager/__init__.py
+++ python/gui/rmanager/__init__.py
@@ -1,11 +1,11 @@
 
 __author__ = "Andruschyshyn Andrey and Kirill Malyshev"
-__copyright__ = "Copyright 2017, Wargaming"
+__copyright__ = "Copyright 2018, Wargaming"
 __credits__ = ["Andruschyshyn Andrey", "Kirill Malyshev"]
 __license__ = "CC BY-NC-SA 4.0"
-__version__ = "3.2.4"
+__version__ = "3.2.6"
 __maintainer__ = "Andruschyshyn Andrey"
-__email__ = "p0lir0id@yandex.ru"
+__email__ = "prn.a_andruschyshyn@wargaming.net"
 __status__ = "Production"
 
 from gui.rmanager.hooks import *
