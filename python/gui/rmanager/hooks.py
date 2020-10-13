de2fd5c926cbf1286d3c5a457df0fa59cee09533
Andrey Andruschyshyn <p0lir0id@yandex.ru>
2020-10-13 18:46:46 +0300

    unused code for login window

--- python/gui/rmanager/hooks.py
+++ python/gui/rmanager/hooks.py
@@ -101,24 +101,6 @@ def __showBattleResults(baseMethod, baseObject, reusableInfo, _, resultsWindow):
 		if arenaBonusType == ARENA_BONUS_TYPE.EPIC_BATTLE:
 			event_dispatcher.showEpicBattlesAfterBattleWindow(reusableInfo, resultsWindow)
 
-# this one for open replays result window in login space
-# its not the best solution, but its work
-# one of solutions was remove replaysmanager from lign window
-from gui.battle_results.components.personal import PremiumInfoBlock
-@override(PremiumInfoBlock, 'getVO')
-def __getVO(baseMethod, baseObject):
-	if BigWorld.player() is None:
-		class FakePlayer:
-			def isInBattleQueue(self):
-				return False
-		realBWPlayer = BigWorld.player
-		BigWorld.player = FakePlayer
-		base = baseMethod(baseObject)
-		BigWorld.player = realBWPlayer
-	else:
-		base =  baseMethod(baseObject)
-	return base
-
 # Add missing battle result fields (creditsReplay, xpReply, freeXpReplay, goldReplay, fortResource, crystalReplay)
 # See BattleReplay.py onBattleResultsReceived method
 
